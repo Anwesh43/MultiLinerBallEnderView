0fae2371bd4b1404752a2f860f7466d0e471a350
Anwesh43 <anweshthecool0@gmail.com>
2020-08-26 03:09:16 +0530

    adding MLBENode class

--- multilineballenderview/src/main/java/com/anwesh/uiprojects/multilineballenderview/MultiLineBallEnderView.kt
+++ multilineballenderview/src/main/java/com/anwesh/uiprojects/multilineballenderview/MultiLineBallEnderView.kt
@@ -123,4 +123,41 @@ class MultiLineBallEnderView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class MLBENode(var i : Int, val state : State = State()) {
+
+        private var next : MLBENode? = null
+        private var prev : MLBENode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = MLBENode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawMLBENode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : MLBENode {
+            var curr : MLBENode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
