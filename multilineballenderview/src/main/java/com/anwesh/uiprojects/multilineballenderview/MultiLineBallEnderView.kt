9ee67263a23fe1abdb785b71b5a82dea95cbff3a
Anwesh43 <anweshthecool0@gmail.com>
2020-08-26 03:14:56 +0530

    adding Renderer class

--- multilineballenderview/src/main/java/com/anwesh/uiprojects/multilineballenderview/MultiLineBallEnderView.kt
+++ multilineballenderview/src/main/java/com/anwesh/uiprojects/multilineballenderview/MultiLineBallEnderView.kt
@@ -187,4 +187,27 @@ class MultiLineBallEnderView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : MultiLineBallEnderView) {
+
+        private val animator : Animator = Animator(view)
+        private val mlbe : MultiLineBallEnder = MultiLineBallEnder(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            mlbe.draw(canvas, paint)
+            animator.animate {
+                mlbe.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            mlbe.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
