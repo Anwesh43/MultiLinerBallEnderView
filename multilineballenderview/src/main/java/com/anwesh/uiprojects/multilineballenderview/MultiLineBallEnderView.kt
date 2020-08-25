453beeae2814ca7951ae8764727628acd26c8362
Anwesh43 <anweshthecool0@gmail.com>
2020-08-26 02:50:34 +0530

    adding constants and libraries

--- multilineballenderview/src/main/java/com/anwesh/uiprojects/multilineballenderview/MultiLineBallEnderView.kt
+++ multilineballenderview/src/main/java/com/anwesh/uiprojects/multilineballenderview/MultiLineBallEnderView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.multilineballenderview
 /**
  * Created by anweshmishra on 26/08/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.Canvas
+
+val colors : Array<String> = arrayOf("#673AB7", "#F44336", "#3F51B5", "#4CAF50", "#2196F3")
+val parts : Int = 4
+val scGap : Float = 0.02f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 2.9f
+val ballFactor : Float = 4.5f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
