f156a33f0e5498fce521af9940a89139c306e743
Anwesh43 <anweshthecool0@gmail.com>
2020-08-26 03:23:38 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedmultilineballenderview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedmultilineballenderview/MainActivity.kt
@@ -2,11 +2,18 @@ package com.anwesh.uiprojects.linkedmultilineballenderview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.anwesh.uiprojects.multilineballenderview.MultiLineBallEnderView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        MultiLineBallEnderView.create(this)
+        fullScreen()
     }
+}
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
